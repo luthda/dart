38541017d2d4a5af27df71c94cdab2494b2ee0c4
luthda <david.luthiger@zuehlke.com>
2022-10-05 09:39:59 +0200

    publish armstrong numbers

--- armstrong-numbers/lib/armstrong_numbers.dart
+++ armstrong-numbers/lib/armstrong_numbers.dart
@@ -1,3 +1,18 @@
 class ArmstrongNumbers {
-  // Put your code here
+  bool isArmstrongNumber(int number) {
+    var digits = number.toString().split('').map(int.parse).toList();
+    var sum = 0;
+    for (var digit in digits) {
+      sum += pow(digit, digits.length);
+    }
+    return sum == number;
+  }
+
+  int pow(int base, int exponent) {
+    var result = 1;
+    for (var i = 0; i < exponent; i++) {
+      result *= base;
+    }
+    return result;
+  }
 }
