06d7dbe56b08ddcd9286bf61b64d894699873e25
luthda <david.luthiger@zuehlke.com>
2022-10-06 08:23:38 +0200

    publish prime-factors

--- prime-factors/lib/prime_factors.dart
+++ prime-factors/lib/prime_factors.dart
@@ -1,3 +1,13 @@
 class PrimeFactors {
-  // Put your code here
+  List<int> factors(int number) {
+    if (number == 1) {
+      return [];
+    }
+    for (int i = 2; i <= number; i++) {
+      if (number % i == 0) {
+        return [i] + factors(number ~/ i);
+      }
+    }
+    return [];
+  }
 }
