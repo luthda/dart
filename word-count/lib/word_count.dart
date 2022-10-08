6b351bfa17776cf69f15afde27277bbf9d286257
luthda <david.luthiger@zuehlke.com>
2022-10-08 14:41:56 +0200

    publish word count

--- word-count/lib/word_count.dart
+++ word-count/lib/word_count.dart
@@ -1,3 +1,11 @@
 class WordCount {
-  // Put your code here
+  Map<String, int> countWords(String phrase) {
+    var arrayOfWords = phrase.split(new RegExp(r'[\s,]+'));
+    var wordCount = new Map<String, int>();
+
+    return arrayOfWords.fold(wordCount, (wordCount, word) {
+      wordCount[word] = wordCount.putIfAbsent(word, () => 0) + 1;
+      return wordCount;
+    });
+  }
 }
