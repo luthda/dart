e2704f08444c1196f214b9db86896e901b31f4a0
luthda <david.luthiger@zuehlke.com>
2022-10-09 10:49:56 +0200

    publish bob

--- bob/lib/bob.dart
+++ bob/lib/bob.dart
@@ -1,3 +1,15 @@
 class Bob {
-  // Put your code here
+  String response(String question) {
+    var trimedQuestion = question.trim();
+
+    if (trimedQuestion.isEmpty) {
+      return 'Fine. Be that way!';
+    } else if (trimedQuestion.endsWith('?')) {
+      return 'Sure.';
+    } else if (trimedQuestion.toUpperCase() == trimedQuestion) {
+      return 'Whoa, chill out!';
+    } else {
+      return 'Whatever.';
+    }
+  }
 }
