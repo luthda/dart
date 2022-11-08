92ecff0b6bdae3c4e01bb8ba2b977c981f482f98
luthda <david.luthiger@zuehlke.com>
2022-11-08 14:05:09 +0100

    publish BST

--- binary-search-tree/lib/binary_search_tree.dart
+++ binary-search-tree/lib/binary_search_tree.dart
@@ -1,3 +1,49 @@
 class BinarySearchTree {
-  // Put your code here
+  final Node root;
+  
+  List<String> get sortedData => _sortData(root);
+
+  BinarySearchTree(String data) : this.root = Node(data);
+
+  void insert(String data, [Node? node]) {
+    node ??= root;
+  
+    if (data.compareTo(node.data) <= 0) {
+      if (node.left == null) {
+        node.left = Node(data);
+      } else {
+        insert(data, node.left);
+      }
+    } else {
+      if (node.right == null) {
+        node.right = Node(data);
+      } else {
+        insert(data, node.right);
+      }
+    }
+  }
+
+  List<String> _sortData(Node node) {
+    final sortedData = <String>[];
+
+    if (node.left != null) {
+      sortedData.addAll(_sortData(node.left!));
+    }
+
+    sortedData.add(node.data);
+
+    if (node.right != null) {
+      sortedData.addAll(_sortData(node.right!));
+    }
+
+    return sortedData;
+  }
+}
+
+class Node {
+  final String data;
+  Node? left;
+  Node? right;
+
+  Node(this.data);
 }
