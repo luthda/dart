892aa19f97f6c639e418a1fd2d16b71351a4cdcc
luthda <david.luthiger@zuehlke.com>
2022-11-10 11:26:48 +0100

    publish nth-prime

--- nth-prime/lib/nth_prime.dart
+++ nth-prime/lib/nth_prime.dart
@@ -1,3 +1,28 @@
+import 'dart:math';
+
 class NthPrime {
-  // Put your code here
+  int prime(int number) {
+    if (number < 1) {
+      throw ArgumentError('There is no zeroth prime');
+    }
+    var candidate = 2;
+
+    for (int count = 0; count < number; candidate++) {
+      if (isPrime(candidate)) {
+        count++;
+      }
+    }
+
+    return candidate - 1;
+  }
+
+  bool isPrime(int number) {
+    for (int i = 2; i <= sqrt(number); i++) {
+      if (number % i == 0) {
+        return false;
+      }
+    }
+
+    return true;
+  }
 }
